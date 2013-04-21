7c32b2edd36b1b3ed3b75ddb06a3c486fa2e1203
Benjamin Peng <j100002ben@gmail.com>
2013-04-22 00:34:03 +0800

    Add test template htmls.

--- phpsass-compile.php
+++ phpsass-compile.php
@@ -9,26 +9,18 @@
 
 header('Content-type: text/css');
 
-require_once './phpsass/SassParser.php';
-
-function warn($text, $context) {
-	print "/** WARN: $text, on line {$context->node->token->line} of {$context->node->token->filename} **/\n";
-}
-function debug($text, $context) {
-	print "/** DEBUG: $text, on line {$context->node->token->line} of {$context->node->token->filename} **/\n";
-}
-
-
-$file = $_SERVER['DOCUMENT_ROOT'] . $_SERVER['PATH_INFO'];
-$syntax = substr($file, -4, 4);
+$cache_path = __DIR__ . '/css/cache/';
+$css_file = $_SERVER['DOCUMENT_ROOT'] . $_SERVER['PATH_INFO'];
+$syntax = substr($css_file, -4, 4);
 $style = isset($_GET['style']) ? $_GET['style'] : 'expanded';
 if(!is_string($style) || !in_array($style, array('expanded', 'nested', 'compact', 'compressed'))){
 	$style = 'expanded';
 }
+$cache_file  = $cache_path . '/' . md5($_SERVER['PATH_INFO']) . ".{$syntax}.{$style}.css";
 
 $options = array(
 	'style' => $style,
-	'cache' => FALSE,
+	'cache' => TRUE,
 	'syntax' => $syntax,
 	'debug' => TRUE,
 	'line_numbers' => TRUE,
@@ -39,9 +31,32 @@ $options = array(
 );
 
 // Execute the compiler.
-$parser = new SassParser($options);
 try {
-	print $parser->toCss($file);
+	if( $options['cache'] ){
+		if( file_modify_time($css_file) > file_modify_time($cache_file) ){
+			include './phpsass/SassParser.php';
+			$parser = new SassParser($options);
+			$css = $parser->toCss($css_file);
+			file_put_contents($cache_file, $css);
+			echo $css;
+		}else{
+			echo file_get_contents($cache_file);
+		}
+	}
 } catch (Exception $e) {
-	print $e->getMessage();	
+	echo $e->getMessage();	
+}
+
+function file_modify_time($file_path)
+{
+	return file_exists($file_path) ? max( @filectime($file_path), @filemtime($file_path) ) : -1;
+}
+
+function warn($text, $context)
+{
+	print "/** WARN: $text, on line {$context->node->token->line} of {$context->node->token->filename} **/\n";
+}
+function debug($text, $context)
+{
+	print "/** DEBUG: $text, on line {$context->node->token->line} of {$context->node->token->filename} **/\n";
 }
